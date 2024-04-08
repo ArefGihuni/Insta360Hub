5601730d972e08aa78e432679ff0367772852d7f
Aref Gihuni <ArefGihuni@users.noreply.github.com>
2024-04-08 18:01:42 +0330

    Create API.cs

---
+++ Insta360.NET.API/API.cs
@@ -0,0 +1,23 @@
+﻿using Insta360Hub.Models.Responses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insta360.NET.API
+{
+    public class API
+    {
+        public Uri BaseUri { get; }
+
+        public API()
+        {
+            BaseUri = new Uri("http://192.168.42.1");
+        }
+
+
+    }
+}
