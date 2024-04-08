0fb82baa1e4aa2793bf7445de8d2171b6b344ad5
Aref Gihuni <ArefGihuni@users.noreply.github.com>
2024-04-08 23:47:58 +0330

    Create StartCapture.cs

---
+++ Insta360.NET.API/Models/Requests/StartCapture.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insta360.NET.API.Models.Requests
+{
+    public class StartCapture
+    {
+        [JsonProperty("name")]
+        public string Name { get; } = "camera.startCapture";
+
+        public string GetJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
