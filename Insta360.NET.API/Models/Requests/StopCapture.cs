cd268d3884e885880d42ea823b2c2519b1cb4d2d
Aref Gihuni <ArefGihuni@users.noreply.github.com>
2024-04-08 23:48:03 +0330

    Create StopCapture.cs

---
+++ Insta360.NET.API/Models/Requests/StopCapture.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insta360.NET.API.Models.Requests
+{
+    public class StopCapture
+    {
+        [JsonProperty("name")]
+        public string Name { get; } = "camera.stopCapture";
+
+        public string GetJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
