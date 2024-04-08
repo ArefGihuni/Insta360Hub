d0c2cfc85ee4bbd53caa4992f337c8c2dc7ad9c5
Aref Gihuni <ArefGihuni@users.noreply.github.com>
2024-04-08 23:46:15 +0330

    Create SetOptionsResponse.cs

---
+++ Insta360.NET.API/Models/Responses/SetOptionsResponse.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insta360.NET.API.Models.Responses
+{
+    public class SetOptionsResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        public bool IsDone
+        {
+            get
+            {
+                return State == "done";
+            }
+        }
+
+        //TODO: other properties
+    }
+}
