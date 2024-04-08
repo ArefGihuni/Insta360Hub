08e85364f7accdb69d1d12831309afc5d835f114
Aref Gihuni <ArefGihuni@users.noreply.github.com>
2024-04-08 23:47:51 +0330

    Create CaptureResponse.cs

---
+++ Insta360.NET.API/Models/Responses/CaptureResponse.cs
@@ -0,0 +1,34 @@
+﻿using Insta360Hub.Models.Responses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insta360.NET.API.Models.Responses
+{
+    public class CaptureResponse
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("progress")]
+        public Progress Progress { get; set; }
+
+
+        public bool IsDone
+        {
+            get
+            {
+                return State == "done";
+            }
+        }
+    }
+}
