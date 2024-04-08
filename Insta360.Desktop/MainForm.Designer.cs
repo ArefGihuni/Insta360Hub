a7d5c441677c08b83465344de2cc99f2b4ccd32b
Aref Gihuni <ArefGihuni@users.noreply.github.com>
2024-04-08 23:57:11 +0330

    desktop

---
+++ Insta360.Desktop/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Insta360.Desktop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            getInfoButton = new Button();
+            responseTextBox = new TextBox();
+            button1 = new Button();
+            setOptionsButton = new Button();
+            hdrCheckBox = new CheckBox();
+            imageRadio = new RadioButton();
+            videoRadio = new RadioButton();
+            startCaptureButton = new Button();
+            takePictureButton = new Button();
+            SuspendLayout();
+            // 
+            // getInfoButton
+            // 
+            getInfoButton.Location = new Point(12, 109);
+            getInfoButton.Name = "getInfoButton";
+            getInfoButton.Size = new Size(142, 57);
+            getInfoButton.TabIndex = 0;
+            getInfoButton.Text = "Get Info";
+            getInfoButton.UseVisualStyleBackColor = true;
+            getInfoButton.Click += getInfoButton_Click;
+            // 
+            // responseTextBox
+            // 
+            responseTextBox.Location = new Point(377, 47);
+            responseTextBox.Multiline = true;
+            responseTextBox.Name = "responseTextBox";
+            responseTextBox.ReadOnly = true;
+            responseTextBox.Size = new Size(336, 358);
+            responseTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(160, 109);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 57);
+            button1.TabIndex = 3;
+            button1.Text = "Get State";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // setOptionsButton
+            // 
+            setOptionsButton.Location = new Point(12, 219);
+            setOptionsButton.Name = "setOptionsButton";
+            setOptionsButton.Size = new Size(142, 57);
+            setOptionsButton.TabIndex = 4;
+            setOptionsButton.Text = "Set Options";
+            setOptionsButton.UseVisualStyleBackColor = true;
+            setOptionsButton.Click += setOptionsButton_Click;
+            // 
+            // hdrCheckBox
+            // 
+            hdrCheckBox.AutoSize = true;
+            hdrCheckBox.Location = new Point(254, 236);
+            hdrCheckBox.Name = "hdrCheckBox";
+            hdrCheckBox.Size = new Size(62, 24);
+            hdrCheckBox.TabIndex = 5;
+            hdrCheckBox.Text = "HDR";
+            hdrCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // imageRadio
+            // 
+            imageRadio.AutoSize = true;
+            imageRadio.Location = new Point(160, 219);
+            imageRadio.Name = "imageRadio";
+            imageRadio.Size = new Size(72, 24);
+            imageRadio.TabIndex = 6;
+            imageRadio.TabStop = true;
+            imageRadio.Text = "Image";
+            imageRadio.UseVisualStyleBackColor = true;
+            // 
+            // videoRadio
+            // 
+            videoRadio.AutoSize = true;
+            videoRadio.Location = new Point(160, 252);
+            videoRadio.Name = "videoRadio";
+            videoRadio.Size = new Size(69, 24);
+            videoRadio.TabIndex = 7;
+            videoRadio.TabStop = true;
+            videoRadio.Text = "Video";
+            videoRadio.UseVisualStyleBackColor = true;
+            // 
+            // startCaptureButton
+            // 
+            startCaptureButton.Location = new Point(160, 323);
+            startCaptureButton.Name = "startCaptureButton";
+            startCaptureButton.Size = new Size(142, 57);
+            startCaptureButton.TabIndex = 8;
+            startCaptureButton.Text = "Start Capture";
+            startCaptureButton.UseVisualStyleBackColor = true;
+            startCaptureButton.Click += startCaptureButton_Click;
+            // 
+            // takePictureButton
+            // 
+            takePictureButton.Location = new Point(12, 323);
+            takePictureButton.Name = "takePictureButton";
+            takePictureButton.Size = new Size(142, 57);
+            takePictureButton.TabIndex = 9;
+            takePictureButton.Text = "Take Picture";
+            takePictureButton.UseVisualStyleBackColor = true;
+            takePictureButton.Click += takePictureButton_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(796, 456);
+            Controls.Add(takePictureButton);
+            Controls.Add(startCaptureButton);
+            Controls.Add(videoRadio);
+            Controls.Add(imageRadio);
+            Controls.Add(hdrCheckBox);
+            Controls.Add(setOptionsButton);
+            Controls.Add(button1);
+            Controls.Add(responseTextBox);
+            Controls.Add(getInfoButton);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button getInfoButton;
+        private TextBox responseTextBox;
+        private Button button1;
+        private Button setOptionsButton;
+        private CheckBox hdrCheckBox;
+        private RadioButton imageRadio;
+        private RadioButton videoRadio;
+        private Button startCaptureButton;
+        private Button takePictureButton;
+    }
+}
---
+++ Insta360.Desktop/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using Insta360.NET.API;
+using Insta360.NET.API.Models.Enums;
+using Insta360.NET.API.Models.Responses;
+using Newtonsoft.Json;
+
+namespace Insta360.Desktop
+{
+    public partial class MainForm : Form
+    {
+        private API Api;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Api = new API();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void getInfoButton_Click(object sender, EventArgs e)
+        {
+            var result = await Api.GetInfoAsync();
+            responseTextBox.Text = JsonConvert.SerializeObject(result);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var result = await Api.GetStateAsync();
+            responseTextBox.Text = JsonConvert.SerializeObject(result);
+        }
+
+        private async void setOptionsButton_Click(object sender, EventArgs e)
+        {
+            var hdr = hdrCheckBox.Checked ? HDR.hdr : HDR.off;
+            SetOptionsResponse result;
+
+            if (videoRadio.Checked)
+            {
+                result = await Api.SetOptionsAsync(CaptureModes._other, hdr);
+            }
+            else if (imageRadio.Checked)
+            {
+                result = await Api.SetOptionsAsync(CaptureModes.image, hdr);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            responseTextBox.Text = JsonConvert.SerializeObject(result);
+
+        }
+
+        private async void takePictureButton_Click(object sender, EventArgs e)
+        {
+            var hdr = hdrCheckBox.Checked ? HDR.hdr : HDR.off;
+            var result = await Api.TakePictureAsync(hdr);
+            responseTextBox.Text = JsonConvert.SerializeObject(result);
+        }
+
+        bool isCapturing = false;
+        private async void startCaptureButton_Click(object sender, EventArgs e)
+        {
+            if (isCapturing)
+            {
+                var result = await Api.StopCaptureAsync();
+
+                if (result.IsDone)
+                {
+                    isCapturing = false;
+                    startCaptureButton.Text = "Start Capture";
+                }
+
+                responseTextBox.Text = JsonConvert.SerializeObject(result);
+            }
+            else
+            {
+                var hdr = hdrCheckBox.Checked ? HDR.hdr : HDR.off;
+                var result = await Api.StartCaptureAsync(hdr);
+
+                if (result.IsDone)
+                {
+                    isCapturing = true;
+                    startCaptureButton.Text = "Stop Capture";
+                }
+
+                responseTextBox.Text = JsonConvert.SerializeObject(result);
+            }
+        }
+
+    }
+}
