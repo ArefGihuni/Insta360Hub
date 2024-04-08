33864c8fcd5553e455f6c860ac6dea7be86351e9
Aref Gihuni <ArefGihuni@users.noreply.github.com>
2024-04-08 23:48:09 +0330

    Create Progress.cs

---
+++ Insta360.NET.API/Models/Responses/Progress.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insta360.NET.API.Models.Responses
+{
+    public class Progress
+    {
+        [JsonProperty("completion")]
+        public int Completion { get; set; }
+    }
+}
